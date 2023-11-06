4357659c08732b1ad52e579e17f16ee010abc459
mohitsh7979 <mohitsh7979@gmail.com>
2023-11-06 19:04:27 +0530

    wrok with drf

---
+++ DRF/myproj_6/myapp/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+class RegisterSerializer(serializers.Serializer):
+
+    username = serializers.CharField()
+    email = serializers.EmailField()
+    password = serializers.CharField()
+     
+    def validate(self, data):
+
+        if data['username']:
+
+            if User.objects.filter(username=data['username']).exists():
+                raise serializers.ValidationError('username already taken')
+            
+        if data['email']:
+
+            if User.objects.filter(email=data['email']).exists():
+                raise serializers.ValidationError('email already taken')
+            
+    def create(self,data):
+        user = User.objects.create(username=data['username'],email=data['email'])
+        user.set_password(data['password'])
+        user.save()
+        return data
+    
+
+class LoginSerializer(serializers.Serializer):
+
+    username = serializers.CharField()
+    password = serializers.CharField()
